c979a1e3e1e2839075fecc12b287caa9801d1994
YourHacktivist <anonymover1@gmail.com>
2025-05-12 14:55:32 +0200

    add signup front end page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import CommunicationPage from "./communication";
 import Privacy from "./Privacy";
 import Terms from "./terms";
+import Signup from "./signup";
 
 import "./index.css";
 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/communication" element={<CommunicationPage />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/terms" element={<Terms />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
